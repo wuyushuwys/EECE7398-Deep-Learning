21c53c0ca42e1348d5b0748b5db75b669c0981d0
wuyushuwys <wuyushuwys@outlook.com>
2020-02-04 18:43:59 -0500

    HW1 template

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -9,22 +9,24 @@ import numpy as np
 """
 Download and load the data
 """
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+def loadData():
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-train_set = torchvision.datasets.CIFAR10(root='./data.cifar10', train=True,
-                                        download=True, transform=transform)
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=10,
-                                          shuffle=True, num_workers=2)
+    train_set = torchvision.datasets.CIFAR10(root='./data.cifar10', train=True,
+                                            download=True, transform=transform)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=10,
+                                              shuffle=True, num_workers=2)
 
-test_set = torchvision.datasets.CIFAR10(root='./data.cifar10', train=False,
-                                       download=True, transform=transform)
-test_loader = torch.utils.data.DataLoader(test_set, batch_size=10,
-                                         shuffle=False, num_workers=2)
+    test_set = torchvision.datasets.CIFAR10(root='./data.cifar10', train=False,
+                                           download=True, transform=transform)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=10,
+                                             shuffle=False, num_workers=2)
 
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    return train_loader, test_loader, classes
 
 
 class Net(nn.Module):
@@ -43,7 +45,7 @@ class Net(nn.Module):
         return x
 
 
-def test(dataset, criterion):
+def test(net, dataset, criterion, device):
     net.eval()
     correct = 0
     total = 0
@@ -56,43 +58,50 @@ def test(dataset, criterion):
             total_loss += criterion(outputs, labels)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
-
-    print('Accuracy of the network on the 10000 test images: %d %%, loss : %.3f' % (
-        100 * correct / total, total_loss/len(dataset)))
+    return 100 * correct / total, total_loss/len(dataset)
 
 
-def train(net, data, criterion, optimizer):
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    net.to(device)
-    print("Using Device %s" % device)
-    for epoch in range(5):  # loop over the dataset multiple times
+def train(net, train_dataset, criterion, optimizer, device):
+    # for epoch in range(10):  # loop over the dataset multiple times
+    # running_loss = 0.0
+    for i, data in enumerate(train_dataset, 0):
+        net.train()
+        # get the inputs; data is a list of [inputs, labels]
+        inputs = data[0].view(-1, np.array(data[0].size()[1:]).prod()).to(device)
+        labels = data[1].to(device)
+        # zero the parameter gradients
+        optimizer.zero_grad()
 
-        running_loss = 0.0
-        for i, data in enumerate(train_loader, 0):
-            net.train()
-            # get the inputs; data is a list of [inputs, labels]
-            inputs = data[0].view(-1, np.array(data[0].size()[1:]).prod()).to(device)
-            labels = data[1].to(device)
-            # zero the parameter gradients
-            optimizer.zero_grad()
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
 
-            # forward + backward + optimize
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-            # print statistics
-            running_loss += loss.item()
-            if i % 1000 == 999:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
-        test(test_loader, criterion)
-    print('Finished Training')
+        # print statistics
+        # running_loss += loss.item()
+        # if i % 1000 == 999:    # print every 2000 mini-batches
+        #     print('[%d, %5d] loss: %.3f' %
+        #           (epoch + 1, i + 1, running_loss / 1000))
+        #     running_loss = 0.0
 
 
 def main():
+    train_loader, test_loader, classes = loadData()
     net = Net()
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(net.parameters(), lr=0.001)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    net.to(device)
+    print("Using Device %s" % device)
+    for epoch in range(1):
+        train(net, train_loader, criterion, optimizer, device)
+        train_accuracy, train_loss = test(net, train_loader, criterion, device)
+        test_accuracy, test_loss = test(net, test_loader, criterion, device)
+        print("### Epoch {}\ttrain accuracy: {}%\ttrain loss: {}\ttest accuracy: {}%\ttest loss:{}"
+              .format(epoch+1, train_accuracy, train_loss, test_accuracy, test_loss))
+
+    print("Finish!")
+
+
+main()
