21d4984bb3125d6a7332fc816e5e42e4728c21a9
wuyushuwys <wuyushuwys@outlook.com>
2020-02-04 18:29:40 -0500

    preliminary version of HW1

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -1,129 +1,98 @@
 import torch
+import torchvision
+import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.utils.data as Data
-import torchvision
-import matplotlib.pyplot as plt
+import torch.optim as optim
 import numpy as np
 
+"""
+Download and load the data
+"""
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-train_data = torchvision.datasets.CIFAR10(
-    root='./data.cifar10',                          # location of the dataset
-    train=True,                                     # this is training data
-    transform=torchvision.transforms.ToTensor(),    # Converts a PIL.Image or numpy.ndarray to torch.FloatTensor of shape (C x H x W)
-    download=True                                   # if you haven't had the dataset, this will automatically download it for you
-)
-train_loader = Data.DataLoader(dataset=train_data,batch_size=10, shuffle=False)
+train_set = torchvision.datasets.CIFAR10(root='./data.cifar10', train=True,
+                                        download=True, transform=transform)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=10,
+                                          shuffle=True, num_workers=2)
 
-test_data = torchvision.datasets.CIFAR10(root='./data.cifar10/', train=False, transform=torchvision.transforms.ToTensor())
-
-test_loader = Data.DataLoader(dataset=test_data, batch_size=10, shuffle=False )
+test_set = torchvision.datasets.CIFAR10(root='./data.cifar10', train=False,
+                                       download=True, transform=transform)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=10,
+                                         shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 
-# functions to show an image
-
-
-# def imshow(img):
-#     img = img / 2 + 0.5     # unnormalize
-#     npimg = img.numpy()
-#     plt.imshow(np.transpose(npimg, (1, 2, 0)))
-#     plt.show()
-
-
-# get some random training images
-# dataiter = iter(train_loader)
-# images, labels = dataiter.next()
-
-# show images
-# imshow(torchvision.utils.make_grid(images))
-# print labels
-# print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
-
-class Net(torch.nn.Module):
+class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        # self.fc1 = torch.nn.Linear(3*32*32, 100)
-        # # self.dropout1 = torch.nn.Dropout(0.5)
-        # self.fc2 = torch.nn.Linear(100, 25)
-        # # self.dropout2 = torch.nn.Dropout(0.25)
-        # self.output = torch.nn.Linear(25, 10)
-
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc1 = torch.nn.Linear(3*32*32, 100)
+        self.fc2 = torch.nn.Linear(100, 25)
+        self.dropout = torch.nn.Dropout(0.1)
+        self.output = torch.nn.Linear(25, 10)
 
     def forward(self, x):
-        # x = F.relu(self.fc1(x))
-        # # x = torch.flatten(self.dropout1(x), 1)
-        # x = F.relu(self.fc2(x))
-        # # x = self.dropout2(x)
-        # x = self.output(x)
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.dropout(x)
+        x = self.output(x)
         return x
 
 
-def test(dataset, loss_func):
+def test(dataset, criterion):
     net.eval()
     correct = 0
     total = 0
+    total_loss = 0
     with torch.no_grad():
-        loss = 0
         for data in dataset:
-            images, labels = data
-            # images = images.view(-1, 32*32*3)
+            images, labels = data[0].view(-1, np.array(data[0].size()[1:]).prod()).to(device), data[1].to(device)
             outputs = net(images)
             _, predicted = torch.max(outputs.data, 1)
-            loss += loss_func(outputs, labels).item()
+            total_loss += criterion(outputs, labels)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
-    print(' Accuracy: {}% , Test Loss: {}'.format(correct/total*100, loss/len(dataset)))
+    print('Accuracy of the network on the 10000 test images: %d %%, loss : %.3f' % (
+        100 * correct / total, total_loss/len(dataset)))
 
 
-net = Net()
-print(net)
+def train(net, data, criterion, optimizer):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    net.to(device)
+    print("Using Device %s" % device)
+    for epoch in range(5):  # loop over the dataset multiple times
 
-optimizer = torch.optim.Adam(net.parameters(), lr=0.001, weight_decay=0.1)
-loss_func = torch.nn.CrossEntropyLoss()
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 0):
+            net.train()
+            # get the inputs; data is a list of [inputs, labels]
+            inputs = data[0].view(-1, np.array(data[0].size()[1:]).prod()).to(device)
+            labels = data[1].to(device)
+            # zero the parameter gradients
+            optimizer.zero_grad()
 
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-for epoch in range(1):  # loop over the dataset multiple times
-    running_loss = 0.0
-    for step, data in enumerate(train_loader, 0):
-        net.train()
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
-        # inputs = inputs.view(-1, np.array(inputs.size()[1:]).prod())
-
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = loss_func(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        # running_loss += loss.item()
-        if step % 2000 == 0:    # print every 2000 mini-batches
-            print('[%d, %5d]' % (epoch + 1, step + 1))
-
-            # print('[%d, %5d] Training loss: %.3f' % (epoch + 1, step + 1, loss.item()))
-            test(train_loader, loss_func)
-            running_loss = 0.0
+            # print statistics
+            running_loss += loss.item()
+            if i % 1000 == 999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+        test(test_loader, criterion)
+    print('Finished Training')
 
 
-print('Finished Training')
+def main():
+    net = Net()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
