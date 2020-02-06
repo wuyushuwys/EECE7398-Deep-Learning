0c6e48afa82a61e615a6fc4628c02109b03a4a59
wuyushuwys <wuyushuwys@outlook.com>
2020-02-06 02:31:38 -0500

    HW1 Finished

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -1,3 +1,4 @@
+#!/home/yushu/.venv/bin/python
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -6,7 +7,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
 import os
-
+import argparse
+from PIL import Image
 
 def load_data(batch_size=128):
     """
@@ -26,8 +28,7 @@ def load_data(batch_size=128):
     test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size,
                                              shuffle=False, num_workers=2)
 
-    classes = ('plane', 'car', 'bird', 'cat',
-               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
     return train_loader, test_loader, classes
 
 
@@ -47,62 +48,97 @@ class Net(nn.Module):
         return x
 
 
-def test(net, dataset, criterion, device):
+def test(net, inputs, device, criterion=None, flag='test'):
     net.eval()
-    correct = 0
-    total = 0
-    total_loss = 0
-    with torch.no_grad():
-        for data in dataset:
-            images, labels = data[0].view(-1, np.array(data[0].size()[1:]).prod()).to(device), data[1].to(device)
-            outputs = net(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total_loss += criterion(outputs, labels)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-    return 100 * correct / total, total_loss/len(dataset)
+    if flag == 'inference':
+        with torch.no_grad():
+            image = inputs.view(-1, np.array(inputs.size()).prod()).to(device)
+            output = net(image)
+            _, predicted = torch.max(output.data, 1)
+            return predicted.int()
+    else:
+        correct = 0
+        total = 0
+        total_loss = 0
+        with torch.no_grad():
+            for data in inputs:
+                images, labels = data[0].view(-1, np.array(data[0].size()[1:]).prod()).to(device), data[1].to(device)
+                outputs = net(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total_loss += criterion(outputs, labels)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        return 100 * correct / total, total_loss/len(inputs)
 
 
-def train(net, train_dataset, criterion, optimizer, device):
-    # for epoch in range(10):  # loop over the dataset multiple times
-    # running_loss = 0.0
-    for i, data in enumerate(train_dataset, 0):
-        net.train()
-        # get the inputs; data is a list of [inputs, labels]
-        inputs = data[0].view(-1, np.array(data[0].size()[1:]).prod()).to(device)
-        labels = data[1].to(device)
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-
-def main():
+def train(device, total_epoch):
     train_loader, test_loader, classes = load_data()
     net = Net()
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(net.parameters(), lr=0.001)
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     net.to(device)
-    print("Using Device %s" % device)
     best_test_accuracy = 0
-    for epoch in range(10):
-        train(net, train_loader, criterion, optimizer, device)
-        train_accuracy, train_loss = test(net, train_loader, criterion, device)
-        test_accuracy, test_loss = test(net, test_loader, criterion, device)
-        print("### Epoch {}\t\ttrain accuracy: {}%\t\ttrain loss: {}\t\ttest accuracy: {}%\t\ttest loss:{}"
-              .format(epoch+1, train_accuracy, train_loss, test_accuracy, test_loss))
+    for epoch in range(total_epoch):
+        for i, data in enumerate(train_loader, 0):
+            net.train()
+            # get the inputs; data is a list of [inputs, labels]
+            inputs = data[0].view(-1, np.array(data[0].size()[1:]).prod()).to(device)
+            labels = data[1].to(device)
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+        train_accuracy, train_loss = test(net, train_loader, device, criterion)
+        test_accuracy, test_loss = test(net, test_loader, device, criterion)
+        print("### Epoch [{}/{}]\t\ttrain accuracy: {}%\t\ttrain loss: {}\t\ttest accuracy: {}%\t\ttest loss:{}"
+              .format(epoch+1,total_epoch, train_accuracy, train_loss, test_accuracy, test_loss))
         if best_test_accuracy < test_accuracy:
             best_test_accuracy = test_accuracy
             if not os.path.isdir('./model'):
                 os.mkdir('./model')
             torch.save(net.state_dict(), "./model/fc.pt")
-            print("### Epoch {}\t\tSave Model fot test accuracy {}% ".format(epoch+1, best_test_accuracy))
+            # print("### Epoch {}\t\tSave Model fot test accuracy {}% ".format(epoch+1, best_test_accuracy))
     print("Finish!")
 
 
-main()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Fully Connected Neural Network')
+    parser.add_argument('mode', type=str, help='Mode: train / test')
+    parser.add_argument('image', type=str, default='', nargs='?', help='Image file in test')
+    parser.add_argument('--epoch', type=int, default=20, help='Total Number of Epoch')
+    arg = parser.parse_args()
+
+    # print(arg.mode, arg.image)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("Using Device %s" % device)
+    if arg.mode == 'train':
+        train(device, arg.epoch)
+    elif arg.mode == 'test':
+        net = Net()
+        net.to(device)
+        net.load_state_dict(torch.load('./model/fc.pt'))
+        print("Load network from ./model/fc.pt")
+        # resize image
+        img = Image.open(arg.image)
+        # resized_img = cv2.resize(cv2.imread(arg.image), (32, 32))
+        # convert image to Tensor
+        transform = transforms.Compose(
+            [transforms.Resize((32, 32)),
+             transforms.ToTensor(),
+             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        image_tensor = transform(img).float()
+        label = test(net, image_tensor, device, flag='inference')
+        classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+        print("Predict: %s" % classes[label])
+
+
+
+
+
+
+
+
