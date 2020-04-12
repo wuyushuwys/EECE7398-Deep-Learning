39667f82c07bee74eadbc9dd735b6906b60c9c9d
wuyushuwys <wuyushuwys@outlook.com>
2020-04-11 22:23:33 -0400

    seq2seq notebook

---
+++ HW3/NMT.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals, print_function, division
+from io import open
+import unicodedata
+import string
+import re
+import random
+
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
